1b24fb48c264f6f01e523af40ffb7e702ab966bb
jhertz <jh.furtado@campus.fct.unl.pt>
2022-12-25 21:22:35 +0000

    changed little thing

--- src/jh/grades/manager/DataBaseConnection.java
+++ src/jh/grades/manager/DataBaseConnection.java
@@ -9,12 +9,13 @@ class DataBaseConnection {
 
     private static final Connection conn;
     private static final String DB_NAME = "file.db";
+    private static final String DB_URL = "jdbc:sqlite" + DB_NAME;
 
     static {
         Connection aux = null;
         try{
             Class.forName("org.sqlite.JDBC");
-            aux = DriverManager.getConnection("jdbc:sqlite:file.db");
+            aux = DriverManager.getConnection(DB_URL);
         }catch (Exception e){
             e.printStackTrace(); // by now :)
         }

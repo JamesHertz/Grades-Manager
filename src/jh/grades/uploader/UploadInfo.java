7a75266b7b58d589453a40c62e817c7ed27224f0
jhertz <jh.furtado@campus.fct.unl.pt>
2022-12-29 12:19:34 +0000

    changed UploadInfo public to package visibility :)

--- src/jh/grades/uploader/UploadInfo.java
+++ src/jh/grades/uploader/UploadInfo.java
@@ -1,6 +1,6 @@
 package jh.grades.uploader;
 
-public abstract class UploadInfo {
+abstract class UploadInfo {
     // TODO: I've been assuming that the course is there, what to do if it doesn't happen to be there?
     private final String course_id;
     public UploadInfo(String course_id){

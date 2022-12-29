f61fcc6d85064aa8789c36660ff1a022ce465b2c
jhertz <jh.furtado@campus.fct.unl.pt>
2022-12-29 18:53:15 +0000

    solve little mess with the exceptions

--- src/jh/grades/uploader/excepctions/UploadException.java
+++ src/jh/grades/uploader/excepctions/UploadException.java
@@ -5,8 +5,12 @@ import jh.grades.uploader.UploadInfo;
 // note: runtime exceptions are used when the program
 // cannot recover from the exception anymore, like index out of bound
 public class UploadException extends Exception{
+    /* DEPRECATED UNTIL I ACTUALLY NEED IT
     public UploadException(String message){
         super(message);
-    }
+    }*/
 
+    public UploadException(){
+        super();
+    }
 }
